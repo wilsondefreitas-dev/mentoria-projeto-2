00168f162ac74d3525fb54c9e6efd319ff382ce3
frontmessa <caio-messa@hotmail.com>
2023-11-01 17:33:00 -0300

    feat:nova condicao jsx

--- src/_components/productBox/index.js
+++ src/_components/productBox/index.js
@@ -3,16 +3,34 @@ import productImg from "./prancha.jpeg"
 import './styles.css';
 
 function productBox() {
+  const odd = true
   const productTitle = "pranca sharpeye"
   const imgWidth = 300
   const label = "adicionar ao carrinho"
+
+   
   return (
+
+    
     <div>
+       
+      
       <img src={productImg} alt="prancha" width={imgWidth} />
       <h3>{productTitle}</h3>
       <button>{label}</button>
+
+      {odd==true ?"produto em estoque " : " produto fora de  estoque" }
+      
+      
+       
+        
+       
+      
+      
+      
     </div>
   );
 }
+
 
 export default productBox;
