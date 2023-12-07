338cc30288048f00a354f51fe73526f7882655fc
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-12-07 14:59:00 -0300

    :recycle: refactor: Refatorando ProdustcList

--- src/_components/ProductsList/index.js
+++ src/_components/ProductsList/index.js
@@ -2,12 +2,11 @@ import { useState } from "react";
 import ProductBox from "../productBox";
 
 function ProductsList() {
-
-/**
- * Agora nós precisamos saber quais items foram adicionados
- * no carrinho. Iremos usar o recurso de mapear uma array 
- * para adicionar varios items na lista e também no carrinho.
- */
+  /**
+   * Agora nós precisamos saber quais items foram adicionados
+   * no carrinho. Iremos usar o recurso de mapear uma array
+   * para adicionar varios items na lista e também no carrinho.
+   */
 
   const [itemsOnCart, setItemsOnCart] = useState(0);
 
@@ -25,16 +24,18 @@ function ProductsList() {
     <>
       <h3>
         Carrinho: {itemsOnCart} {itemsOnCart === 1 ? "item" : "items"}
-        <ButtonRemove itemsOnCart={itemsOnCart} handleRemoveItem={handleRemoveItem}/>
+        <ButtonRemove
+          itemsOnCart={itemsOnCart}
+          handleRemoveItem={handleRemoveItem}
+        />
       </h3>
       <ProductBox itemsOnCart={itemsOnCart} addToCart={handleAddToCart} />
     </>
   );
 }
 
-function ButtonRemove({itemsOnCart, handleRemoveItem}) {
-
-const removeItemsButtonDisabled = (itemsOnCart === 0);
+function ButtonRemove({ itemsOnCart, handleRemoveItem }) {
+  const removeItemsButtonDisabled = itemsOnCart === 0;
   return (
     <button disabled={removeItemsButtonDisabled} onClick={handleRemoveItem}>
       Remover item
